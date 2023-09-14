6655fd26d12f788e51271dd381cbce1d1e855a7f
Enes ERKEN <121829291+Edinbo@users.noreply.github.com>
2023-09-14 18:57:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pytube as pt
 import math
 from os import system
-import os
+import pathlib
 
 # ignore
-millnames = ['',' Thousand',' Million',' Billion',' Trillion']
-desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
+millnames = ['', ' Thousand', ' Million', ' Billion', ' Trillion']
+desktop = pathlib.Path.home() / 'Desktop'
 
 # Definitions
 def purple(text):
-    system(""); faded = ""
+    system("")
+    faded = ""
     red = 122
     for character in text:
         red += 3
@@ -19,11 +20,13 @@ def purple(text):
     return faded
 
 def white(text):
-    faded = "\033[97m"
+    faded = "\033[97m" + text + "\033[0m"
     return faded
 
+
 def red(text):
-    system(""); faded = ""
+    system("")
+    faded = ""
     green = 150
     for character in text:
         green -= 5
@@ -34,10 +37,10 @@ def red(text):
 
 def millify(n):
     n = float(n)
-    millidx = max(0,min(len(millnames)-1,
-                        int(math.floor(0 if n == 0 else math.log10(abs(n))/3))))
+    millidx = max(0, min(len(millnames) - 1,
+                        int(math.floor(0 if n == 0 else math.log10(abs(n)) / 3))))
 
-    return '{:.2f}{}'.format(n / 10**(3 * millidx), millnames[millidx])
+    return '{:.2f}{}'.format(n / 10 ** (3 * millidx), millnames[millidx])
 
 def convert(seconds):
     seconds = seconds % (24 * 3600)
@@ -48,44 +51,58 @@ def convert(seconds):
 
     return "%d:%02d:%02d" % (hour, minutes, seconds)
 
+def video(yt):
+    video_resolutions = []
+    for video in yt.streams.filter(progressive=True):
+        video_resolutions.append(video.resolution)
+
+    print("Available Resolutions (mp4/video): ")
+    for res in video_resolutions:
+        print(str(res))
+
+def audio(yt):
+    audio_resolutions = []
+    for audio in yt.streams.filter(progressive=True):
+        audio_resolutions.append(audio.resolution)
+
+    print("Available Resolutions (mp3/audio): ")
+    for res in audio_resolutions:
+        print(str(res))
+
 def download(x):
     try:
         yt = pt.YouTube(x)
-        video_resolutions = []
+        audio(yt)
+        video(yt)
 
-        for video in yt.streams.filter(progressive=True, only_video=True):
-            video_resolutions.append(video.resolution)
+        print(
+            f"//////////////////////\n {red('1 : download highest resolution for video')} \n {red('2 : download highest resolution for audio')} \n {red('3 : download both (video/audio)')}")
+        answer = int(input(red("Select something : ")))
 
-        print("Available Resolutions (mp4/video): ")
-        for res in video_resolutions:
-            print(str(res))
-
-        audio_resolutions = []
-        for audio in yt.streams.filter(progressive=True, only_audio=True):
-            audio_resolutions.append(audio.resolution)
-
-        print("Available Resolutions (mp3/audio): ")
-        for res in audio_resolutions:
-            print(str(res))
-
-        print(f"//////////////////////\n {red('1 : download highest resolution for video')} \n {red('2 : download highest resolution for audio')} \n {red('3 : download all')}")
-        answer = input("Select something : ")
         if answer == 1:
             only_vid = yt.streams.filter(only_audio=False, progressive=True, file_extension='mp4').order_by('resolution').desc().first()
             only_vid.download(desktop)
-        if answer == 2:
-            only_audio = yt.streams.filter(only_audio=True, file_extension='mp3').order_by('abr').desc().first()
+            print(f"Succesfully downloaded {only_vid} to {desktop}")
+            print(purple("Thanks for using / Github : Edinbo"))
+        elif answer == 2:
+            only_audio = yt.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').desc().first()
             only_audio.download(desktop)
-        if answer == 3:
-            all_vids = yt.streams.all()
-            all_vids.download(desktop)
+            print(f"Succesfully downloaded {only_audio} to {desktop}")
+            print(purple("Thanks for using / Github : Edinbo"))
+        elif answer == 3:
+            both_audio = yt.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').desc().first()
+            both_video = yt.streams.filter(only_audio=False, progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            both_audio.download(desktop)
+            both_video.download(desktop)
+            print(f"Succesfully downloaded {both_video, both_audio} to {desktop}")
+            print(purple("Thanks for using / Github : Edinbo"))
     except:
-        print(red("Try downloading again it seems like it didnt work"))
-        x = input("Lets try using the same youtube link : ")
+        print(red("Try downloading again, it seems like it didn't work"))
+        x = input(purple("Let's try using the same YouTube link : "))
         download(x)
 
 def question():
-    download_answer = input(purple("Do you want do download this video? (y/n)"))
+    download_answer = input(purple("Do you want to download this video? (y/n)"))
     if download_answer.lower() != "y":
         print(purple("Thanks for using / Github : Edinbo"))
         exit()
@@ -96,17 +113,12 @@ def main(video_link):
     try:
         yt = pt.YouTube(video_link)
         title, author, views, rating, channel_id, channel_url, publish_date, video_length, thumbnail = yt.title, yt.author, yt.views, yt.rating, yt.channel_id, yt.channel_url, yt.publish_date, yt.length, yt.thumbnail_url
-        print(f" {red('Video Title')} : {white(title)} \n {red('Author/Creator')} : {white(author)} \n {red('Total Views')} : {white(millify(views))} \n {red('Video/Song Length(h,m,s)')} : {white(convert(video_length))} \n {red('Publish Date')} : {white(publish_date)} \n {red('Rating')} : {white(rating)} \n {red('Thumbnail')} : {white(thumbnail)} \n {red('Channel ID')} : {white(channel_id)} \n {red('Channel URL')} : {white(channel_url)}")
+        print(f" {red('Video Title')} : {white(str(title))} \n {red('Author/Creator')} : {white(str(author))} \n {red('Total Views')} : {white(str(millify(views)))} \n {red('Video/Song Length(h,m,s)')} : {white(str(convert(video_length)))} \n {red('Publish Date')} : {white(str(publish_date))} \n {red('Rating')} : {white(str(rating))} \n {red('Thumbnail')} : {white(str(thumbnail))} \n {red('Channel ID')} : {white(str(channel_id))} \n {red('Channel URL')} : {white(str(channel_url))}")
     except:
         print(red("Invalid Link!"))
-        main(input(purple("Youtube video link : ")))
-
-
+        main(input(purple("YouTube video link : ")))
 
 # main functionality
-video_link = input(purple("Youtube video link : "))
+video_link = input(purple("YouTube video link : "))
 main(video_link)
 question()
-
-
-
